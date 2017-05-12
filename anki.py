67938ce99319c77c8e4e889231e997557f6119c9
wribbe <eng_steff@hotmail.com>
2017-05-12 16:55:32 +0200

    Possible to generate LaTeX equations with \L <equation> \L.

--- anki.py
+++ anki.py
@@ -2,17 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import os
 import re
+import subprocess
+import shutil
+import tempfile
 
-lines =  [line.strip('\n') for line in open(sys.argv[1]).readlines()]
-
-#remove indentation
-lines = [re.sub(r'^\s*', '', line) for line in lines]
-
-#remove comments
-lines = [line for line in lines if not line.startswith('#')]
-
-data = []
+dir_images = ""
+current_image = 0
+filename = ""
+media_path = "/home/steff/.local/share/Anki2/User 1/collection.media"
 
 def compress_empty_lines(lines):
     last_was_empty = False
@@ -52,34 +51,88 @@ def fig(string):
     figurename = string.split("{")[-1].replace("}",'')
     return '<img src="{}" />'.format(figurename)
 
+def latex(string):
+    global current_image
 
-lines = remove_comment_blocks(lines)
-lines = compress_empty_lines(lines)
+    def wrap(code):
+        return "\documentclass{standalone}"+\
+        "\\begin{document}"+\
+            "${}$".format(code)+\
+        "\end{document}"
 
-current_question = []
-current_text = []
+    latex_code_find = re.search("\\\\L.*\\\\L", string)
+    latex_code = latex_code_find.group().replace("\\L","").strip()
 
-for line in lines:
-    if line == "/split":
-        line = 3*"="
-    if "fig{" in line:
-        line = fig(line)
-    if not line.strip():
+    if not os.path.exists(dir_images):
+        os.mkdir(dir_images)
+
+    dir_old = os.getcwd()
+    os.chdir(dir_images)
+    with open(os.devnull, 'w') as FNULL:
+        subprocess.check_call(["pdflatex", wrap(latex_code)],
+                               stdout=FNULL,
+                               stderr=subprocess.STDOUT)
+    output_name = "{}_{:03d}.svg".format(filename.split('.')[0], current_image)
+    output_path = os.path.join(media_path, output_name)
+    subprocess.check_call(["pdf2svg", "standalone.pdf", output_path])
+
+    img_path = fig(output_name)
+    string = string.replace(latex_code_find.group(), img_path)
+    current_image += 1
+    os.chdir(dir_old)
+
+    return string
+
+
+def main():
+
+    global filename, dir_images
+    filename = sys.argv[1]
+    dir_images = tempfile.mkdtemp()
+
+    lines =  [line.strip('\n') for line in open(filename).readlines()]
+
+    #remove indentation
+    lines = [re.sub(r'^\s*', '', line) for line in lines]
+
+    #remove comments
+    lines = [line for line in lines if not line.startswith('#')]
+
+    data = []
+    lines = remove_comment_blocks(lines)
+    lines = compress_empty_lines(lines)
+
+    current_question = []
+    current_text = []
+
+    for line in lines:
+        if line == "/split":
+            line = 3*"="
+        if "fig{" in line:
+            line = fig(line)
+        if "\L" in line:
+            line = latex(line)
+        if not line.strip():
+            question = "".join(current_question)
+            answer = "".join(current_text)
+            data.append((question, answer))
+            current_question = []
+            current_text = []
+        elif line.startswith('--'):
+            current_question = current_text
+            current_text = []
+        else:
+            current_text.append(tag(line))
+
+    if current_text:
         question = "".join(current_question)
         answer = "".join(current_text)
         data.append((question, answer))
-        current_question = []
-        current_text = []
-    elif line.startswith('--'):
-        current_question = current_text
-        current_text = []
-    else:
-        current_text.append(tag(line))
 
-if current_text:
-    question = "".join(current_question)
-    answer = "".join(current_text)
-    data.append((question, answer))
+    for question, answer in data:
+        print("{}\t{}".format(question, answer))
 
-for question, answer in data:
-    print("{}\t{}".format(question, answer))
+    shutil.rmtree(dir_images)
+
+if __name__ == "__main__":
+    main()
